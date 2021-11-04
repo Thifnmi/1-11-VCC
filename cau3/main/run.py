480309c82761913a1804a8bb99561100190efb9c
Thifnmi <tuthin2k@gmail.com>
2021-11-04 14:26:17 +0700

    2:25pm 4-11-2021

--- cau3/main/run.py
+++ cau3/main/run.py
@@ -1,8 +1,6 @@
-from utils.util import house, require
+from utils.util import house
 import sys
 sys.path.append("..")
 
 
-test = require(house, ["tu van thin", "nguyen duc thien",
-               "vu trong dat"], "dau tuan duong")
-print(test)
+house(["tu van thin", "nguyen duc thien", "vu trong dat"], "tu van thin1")
--- cau3/utils/util.py
+++ cau3/utils/util.py
@@ -1,16 +1,16 @@
-def house(list_name: list, name: str):
+def require(original_function):
+    def check_name(list_name, name):
+        if name in list_name:
+            print(f"ten {name} du dieu kien duoc vao nha")
+        print(original_function(list_name, name))
+    return check_name
+
+
+@require
+def house(list_name, name):
     if name in list_name:
-        return "ten {} thoa man dieu kien duoc vao nha". format(name)
-    #     return True
-    # return False
-
-
-def require(func, list_name: list, name: str) -> bool:
-    result = func(list_name, name)
-    if result:
         return True
     return False
-    # return (result)
 
-# print(house(["aaa", "bbb", "ccc"], "ccc"))
-# print(require(house, ["aaa", "bbb", "ccc"], "ccc"))
+
+# house(["aaa", "bbb", "ccc"], "ccc")
